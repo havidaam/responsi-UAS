637144c31b478d6bf2afc599841b38a5e90081ec
havidaam <62041982+havidaam@users.noreply.github.com>
2020-07-08 15:30:44 +0700

    Add files via upload

---
+++ Penjualan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectPenjualan
+{
+    public class Penjualan
+    {
+        public abstract string Nota {get; set;}
+        public abstract string Tanggal {get; set;}
+        public abstract string Costumer {get; set;}
+        public abstract string Pembayaran {get; set;}
+        public abstract double TotalNota ();
+        // PERINTAH: lengkapi property class penjualan, sesuai petunjuk soal
+    }
+}
